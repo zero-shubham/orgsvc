30c1314b17eab8eff59ce97f98b530e69d29ba29
Shubham Biswas <shubhambiswas_zero@yahoo.com>
2025-04-24 11:49:04 +0530

    fix lifespan

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from contextlib import asynccontextmanager
-from src.db import init_db, run_migrations, DATABASE_URL
-from fastapi import FastAPI
 from src.application import app
 from opentelemetry.instrumentation.asyncpg import AsyncPGInstrumentor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -18,18 +15,13 @@ FastAPIInstrumentor().instrument_app(app=app)
 LoggingInstrumentor().instrument()  # set OTEL_PYTHON_LOG_CORRELATION=true
 
 provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(processor)
+# processor = BatchSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(processor)
 
 # Sets the global default tracer provider
 trace.set_tracer_provider(provider)
 
 # Creates a tracer from the global tracer provider
-tracer = trace.get_tracer("my.tracer.name")
+tracer = trace.get_tracer("orgsvc")
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_db()
-    await run_migrations("./migrations", DATABASE_URL)
-    yield
--- src/application.py
+++ src/application.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from src.api import api_router
+from contextlib import asynccontextmanager
+from src.db import init_db, run_migrations, DATABASE_URL
+from time import sleep
+from src.logger import get_logger
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    sleep(5)
+    await init_db()
+    get_logger().info("running migrations..")
+    try:
+      await run_migrations("./migrations", DATABASE_URL)
+    except Exception as e:
+      get_logger().info("failed to migrate..", exception=e)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(api_router)
