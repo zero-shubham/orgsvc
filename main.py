7ce7528a406244229a7353cdeaadf1ba12b5663a
Shubham Biswas <shubhambiswas_zero@yahoo.com>
2025-05-27 22:41:49 +0530

    tracing and metrics

--- main.py
+++ main.py
@@ -2,26 +2,61 @@ from src.application import app
 from opentelemetry.instrumentation.asyncpg import AsyncPGInstrumentor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry import trace
+from opentelemetry import trace, metrics
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
+    BatchSpanProcessor
+)
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from fastapi import Request
+
+
+from src.config import OLTP_HTTP_TRACE_ENDPOINT, OLTP_HTTP_METER_ENDPOINT
+
+
+resource = Resource.create({
+    "service.name": "orgsvc"
+})
+
+trace_provider = TracerProvider(
+    resource=resource,
+)
+
+meter_provider = MeterProvider(
+    metric_readers=[PeriodicExportingMetricReader(
+        exporter=OTLPMetricExporter(endpoint=OLTP_HTTP_METER_ENDPOINT),
+        export_interval_millis=1000  # Export every 3 seconds
+    )],
+    resource=resource,
+)
+metrics.set_meter_provider(meter_provider=meter_provider)
+
+processor = BatchSpanProcessor(
+    OTLPSpanExporter(endpoint=OLTP_HTTP_TRACE_ENDPOINT))
+trace_provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(trace_provider)
+
+
+meter = metrics.get_meter("orgsvc", meter_provider=meter_provider)
+
+req_counter = meter.create_counter(
+    "request_counter", unit="1", description="Counts the  total requests served"
 )
 
 
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    req_counter.add(1, {"request.url": request.url.path})
+    return await call_next(request)
+
+
 AsyncPGInstrumentor().instrument()
-FastAPIInstrumentor().instrument_app(app=app)
+FastAPIInstrumentor().instrument_app(
+    app=app, tracer_provider=trace_provider, meter_provider=meter_provider)
 LoggingInstrumentor().instrument()  # set OTEL_PYTHON_LOG_CORRELATION=true
-
-provider = TracerProvider()
-# processor = BatchSpanProcessor(ConsoleSpanExporter())
-# provider.add_span_processor(processor)
-
-# Sets the global default tracer provider
-trace.set_tracer_provider(provider)
-
-# Creates a tracer from the global tracer provider
-tracer = trace.get_tracer("orgsvc")
-
-
--- src/application.py
+++ src/application.py
@@ -5,19 +5,19 @@ from src.db import init_db, run_migrations, DATABASE_URL
 from time import sleep
 from src.logger import get_logger
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     sleep(5)
     await init_db()
     get_logger().info("running migrations..")
     try:
-      await run_migrations("./migrations", DATABASE_URL)
+        await run_migrations("./migrations", DATABASE_URL)
     except Exception as e:
-      get_logger().info("failed to migrate..", exception=e)
+        get_logger().info("failed to migrate..", exception=e)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-
 
 app.include_router(api_router)
